ee060d7e7ebfc49f6cade9eb2262add76b1c3e30
BomberBeetle <gdreks@gmail.com>
2022-12-08 00:37:26 -0300

    wtf, doesnt work on actual input

--- day7/day7part1.hs
+++ day7/day7part1.hs
@@ -6,7 +6,7 @@ import Data.List.Split
 data File =
     File {
         fname :: String,
-        size :: Int
+        size :: Integer
     } deriving (Show)
 
 data Dir =
@@ -19,12 +19,11 @@ data Dir =
 dirNameEquals str _dir = str == name _dir
 fileNameEquals str _file = str == fname _file
 
-processCommands rest tree = foldl commandString tree rest
+processCommands cmds tree = foldl commandString tree cmds
 
 cd tree str
     | active tree = Dir (name tree) (appendAndActivate str (dirs tree)) (files tree) False
-    | not (active tree) && null (dirs tree) = tree
-    | otherwise = Dir (name tree) [cd x str | x <- dirs tree ] (files tree) (active tree)
+    | otherwise = Dir (name tree) [cd x str | x <- dirs tree ] (files tree) False
 
 appendAndActivate str _dirs
     | isNothing (find (dirNameEquals str) _dirs) = Dir str [] [] True:_dirs
@@ -38,22 +37,35 @@ appendDirToActive tree str
 
 appendFileToActive :: Dir -> File -> Dir
 appendFileToActive tree file
-    | active tree = if null (find (fileNameEquals (fname file)) (files tree)) then Dir (name tree) (dirs tree) ((File (fname file) (size file)):(files tree)) (active tree) else tree
-    | not (active tree) && null (dirs tree) = tree
+    | active tree = Dir (name tree) (dirs tree) ((File (fname file) (size file)):(files tree)) (active tree)
     | otherwise = Dir (name tree) [appendFileToActive x file | x <- dirs tree] (files tree) (active tree)
 
 propagateDeactivation _dirs = [Dir (name x) (propagateDeactivation(dirs x)) (files x) False | x <- _dirs]
 
 resetTreeToRoot tree = Dir (name tree) (propagateDeactivation(dirs tree)) (files tree) True
 
-commandString tree "$ ls" = tree
-commandString tree ('$':' ':'c':'d':' ':rest) = if rest=="/" then resetTreeToRoot tree else cd tree rest
-commandString tree ('d':'i':'r':' ':rest) = appendDirToActive tree rest
-commandString tree x = appendFileToActive tree (File  (splitOn " " x !! 1) (read (splitOn " " x !! 0) :: Int))
+goBackOne tree 
+    | not (null (find active (dirs tree))) = Dir (name tree) [ Dir (name x) (dirs x) (files x) False | x <- dirs tree] (files tree) True
+    | active tree = tree
+    | otherwise = Dir (name tree) [goBackOne x | x <- dirs tree] (files tree) False
 
-getDirVals string = processCommands string (Dir "/" [] [] True)
+commandString tree "$ ls" = tree
+commandString tree "$ cd .." = goBackOne tree
+commandString tree "$ cd /" = resetTreeToRoot tree
+commandString tree ('$':' ':'c':'d':' ':rest) = cd tree rest
+commandString tree ('d':'i':'r':' ':rest) = tree
+commandString tree x = appendFileToActive tree (File  (splitOn " " x !! 1) (read (splitOn " " x !! 0) :: Integer))
+
+makeFullTree string = processCommands string (Dir "/" [] [] True)
+
+getDirVals tree = (sum [size x | x <- files tree] + sum res):res
+    where res = concat [getDirVals d | d <- dirs tree]
+
+getDirSum str = sum [x | x <- getDirVals (makeFullTree str), x <= 100000] 
 
 
 main = do
-    stuff <- readFile "./input.txt"
-    print (show (getDirVals (lines stuff)))
+    stuff <- readFile "./expect33.txt"
+    print (show (makeFullTree (lines stuff)))
+    print (show (getDirVals(makeFullTree (lines stuff))))
+    print (show (getDirSum (lines stuff)))
